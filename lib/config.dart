328f311c092d78218ef6deeeac7ff04053bda17e
MNaufalRamdn <98917737+Radhan015@users.noreply.github.com>
2026-06-13 16:43:46 +0700

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -3,5 +3,5 @@ class AppConfig {
   // Android Emulator: 10.0.2.2
   // Device fisik: IP lokal komputer (misal: 192.168.1.5)
   static const String baseUrl = 'http://172.30.73.131:8000/api';
-  static const String storageUrl = 'http:/172.30.73.131:8000/storage';
+  static const String storageUrl = 'http://172.30.73.131:8000/storage';
 }
